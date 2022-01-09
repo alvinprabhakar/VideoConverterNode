1d15a54bd9d30ee47acc7b37198c8760eb565c05
Alvin Prabha <alvinprabhakar@gmail.com>
2022-01-09 16:28:45 +0530

    Netlify

--- services/video.services.js
+++ services/video.services.js
@@ -1,36 +1,36 @@
-const Converter = require('../ffmpeg');
+// const Converter = require('../ffmpeg');
 
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+// const ffmpeg = require('fluent-ffmpeg');
+// const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 
 
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+// ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 
 const service = {
 
     uploadVideo(req,res){
         console.log("Node Post Called");
-    //     if(req.files === undefined){
-    //     return res.status(400).json({msg: 'No file Uploaded'})
-    //     }
-    //     const file = req.files.file;
-    //     const folder = req.body.folder;
-    //     folder.replace(/[\_]/g,'/')
-    //     console.log("Folder" , folder , "File" , file.name);
-    //     console.log(__dirname);
-    //     file.mv(`${__dirname}/${file.name}`, err => {
-    //         if(err){
-    //             console.error(err);
-    //             return res.status(500).send(err);
-    //         }
-    //         res.json({fileName: file.name, filepath: `uploads/${file.name}`});
-    //         ffmpeg(`${__dirname}/${file.name}`).output(`${folder}/output.m3u8`).on('end', () => {
-    //                 console.log('end');
-    //                 let result = {"data" : "Conversion Completed!!!!"}
-    //                 console.log(JSON.stringify(result));
-    //         }).run();   
-    //     })
+        if(req.files === undefined){
+        return res.status(400).json({msg: 'No file Uploaded'})
+        }
+        const file = req.files.file;
+        const folder = req.body.folder;
+        folder.replace(/[\_]/g,'/')
+        console.log("Folder" , folder , "File" , file.name);
+        console.log(__dirname);
+        file.mv(`${__dirname}/${file.name}`, err => {
+            if(err){
+                console.error(err);
+                return res.status(500).send(err);
+            }
+            res.json({fileName: file.name, filepath: `uploads/${file.name}`});
+            ffmpeg(`${__dirname}/${file.name}`).output(`${folder}/output.m3u8`).on('end', () => {
+                    console.log('end');
+                    let result = {"data" : "Conversion Completed!!!!"}
+                    console.log(JSON.stringify(result));
+            }).run();   
+        })
      }
     }
   
--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const serverless = require('serverless-http');
+const fileUpload = require('express-fileupload');
+const videoRouters = require('../routes/video.routes');
 
 const cors = require('cors')
 
 const app = express();
+
+app.use(fileUpload());
+app.use(cors());
+app.use(express.json());
 
 const router = express.Router();
 
@@ -14,10 +20,27 @@ router.get('/',(req,res) => {
 }
 )
 
+app.use('/.netlify/functions/api/upload',videoRouters)
+
 app.use('/.netlify/functions/api', router);
+
+
 
 
 
 //app.listen(process.env.PORT || port);
 
 module.exports.handler = serverless(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
